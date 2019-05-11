e847e9ac3ed0eba18947d0268b300ce070b0b941
Victor Johnson <johnsonsirv@gmail.com>
2019-05-11 12:59:39 +0200

    added the enqueue method to add item to end of the queue, alias is <push>

--- Queue.rb
+++ Queue.rb
@@ -7,4 +7,12 @@ class Queue
     def initialize
         @list = SinglyLinkedList.new
     end
+
+    #push method adds item to the end of the queue. alias is <enqueue>
+    def push(number)
+        @list.push(number)
+    end
+    def enqueue(number)
+        @list.push(number)
+    end
 end
