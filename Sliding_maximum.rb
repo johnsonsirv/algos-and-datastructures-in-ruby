810c4ef861fd5044e8b798bcb678c270bec95271
Victor Johnson <johnsonsirv@gmail.com>
2019-05-12 02:49:45 +0200

    added code logic to sliding_max method

--- Sliding_maximum.rb
+++ Sliding_maximum.rb
@@ -1,6 +1,9 @@
+=begin
+    For every k continuous numbers in an array, print the  largest number.
+=end
 def sliding_max(k, arr)
     # arr.each_cons(k).map(&:max)
-    arr.each_cons(k).map(){|val| val.max}
+    return arr.each_cons(k).map(){|val| val.max}
 end
 
 print sliding_max(3, [1, 3, 5, 7, 9, 2])
