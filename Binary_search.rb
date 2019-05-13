5837f89c454afaea3eee3925f4936c413e62c2fa
Victor Johnson <johnsonsirv@gmail.com>
2019-05-13 10:43:26 +0200

    retrun invalid input for negative squareroot numbers

--- Binary_search.rb
+++ Binary_search.rb
@@ -1,31 +1,9 @@
-# def sqrt(number)
-#     sqrt_recursive(number, 0, number)
-# end
-  
-# def sqrt_recursive(number, min_interval, max_interval)
-#     midpoint = (min_interval + max_interval) / 2
-#     check = midpoint**2
-#     if check == number
-#         return midpoint
-#     elsif check > number
-#         max_interval = midpoint
-#         sqrt_recursive(number, min_interval, max_interval)
-#     elsif check < number
-#         min_interval = midpoint
-#         sqrt_recursive(number, min_interval, max_interval)
-#     end
-
-# end
-  
-# # puts sqrt(25)
-# puts sqrt(7056)
-
 def sqrt(number)
-    sqrt_recursive(number, 0, number)
+    return sqrt_recursive(number, 0, number) if number.positive?
+    return 'Invalid Input'
   end
   
   def sqrt_recursive(number, min_interval, max_interval)
-    # Your code here
       midpoint = (min_interval + max_interval) / 2
       check = midpoint**2
       return midpoint if check.eql? number
@@ -34,15 +12,9 @@ def sqrt(number)
       else
         sqrt_recursive(number, midpoint, max_interval)
       end
-      # elsif check > number
-      #     max_interval = midpoint
-      #     sqrt_recursive(number, min_interval, max_interval)
-      # elsif check < number
-      #     min_interval = midpoint
-      #     sqrt_recursive(number, min_interval, max_interval)
-      # end
   end
   
-  puts sqrt(4)
+  #Test Cases
+  puts sqrt(-25)
   puts sqrt(7056)
   
