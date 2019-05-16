52d206ef910e1a49cf046c1684c7e20b946720ea
Victor Johnson <johnsonsirv@gmail.com>
2019-05-16 18:50:39 +0200

    initial commit for method to backtrace recursion

---
+++ Backtracing_recursion.rb
@@ -0,0 +1,6 @@
+=begin
+    Given a number k and a list of coins, determine if the coins can be added together to equal k. Print true if you can get the exact sum and false otherwise.
+=end
+def exact_sum?(k, coins)
+    #write your code here
+end
