f8cb2d262bd7a58a4062a2bc6a9cfb3ea6e2ef7c
Victor Johnson <johnsonsirv@gmail.com>
2019-05-17 10:37:12 +0200

    added code logic to determine compliments of each coin

--- Backtracing_recursion.rb
+++ Backtracing_recursion.rb
@@ -3,4 +3,31 @@
 =end
 def exact_sum?(k, coins)
     #write your code here
-end
+    if coins.length == 0
+      return k==0 ? true : false
+    else
+      print coins
+      puts k
+      exact_sum?(k-coins[coins.length - 1], coins.tap(&:pop))
+    end
+  end
+  $compliments = []
+  def find_compliments(k, coins)
+      if coins.length == 0
+          return $compliments
+      else
+          $compliments <<  k - coins[coins.length-1]
+          find_compliments(k, coins.tap(&:pop))
+      end
+  end
+  print find_compliments(201, [37, 42, 10, 23, 15, 25, 11, 6, 19, 25, 51])
+  # puts exact_sum?(12, [1, 2, 3, 4, 5])
+  # => true
+  
+  # puts exact_sum?(11, [1, 5, 9, 13])
+  # => false
+  
+#   puts exact_sum?(201, [37, 42, 10, 23, 15, 25, 11, 6, 19, 25, 51])
+#   compliments =       [164, 159, 191, 178, 176, 190, 195, 176, 150]
+  # =>true
+  
