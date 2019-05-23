d5adfff790486a79d3d00a66712ae02f5351742f
Victor Johnson <johnsonsirv@gmail.com>
2019-05-23 09:45:24 +0200

    added test cases to evaluate the insert method and pre_order methods i created

--- Binary_Search_Tree.rb
+++ Binary_Search_Tree.rb
@@ -66,13 +66,13 @@ end
 
 
 tree = BinarySearchTree.new
-tree.insert(50)
-tree.insert(25)
-tree.insert(75)
-tree.insert(15)
-tree.insert(21)
-tree.insert(62)
-tree.insert(91)
+tree.insert(5)
+tree.insert(3)
+tree.insert(7)
+tree.insert(1)
+tree.insert(2)
+tree.insert(6)
+tree.insert(8)
 # tree.insert(4)
 # tree.insert(7)
 # tree.insert(13)
