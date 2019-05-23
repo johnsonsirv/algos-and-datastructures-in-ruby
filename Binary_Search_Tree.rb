e4818ec3f0933aff6454e8abff1243f10734eba9
Victor Johnson <johnsonsirv@gmail.com>
2019-05-23 09:41:29 +0200

    Added a depth_first search pre_order method to return nodes in the tree

--- Binary_Search_Tree.rb
+++ Binary_Search_Tree.rb
@@ -39,4 +39,50 @@ class BinarySearchTree
         end
             
     end
+
+    def pre_order
+        # if node == nil
+        #   return ''
+        # end
+      
+        # result = "#{node.value} "
+        # result += pre_order(node.left)
+        # result += pre_order(node.right)
+        @result = []
+        @current = @root
+        def traverse(node)
+            @result.push(node.value)
+            if node.left
+                traverse(node.left)
+            end
+            if node.right
+                traverse(node.right)
+            end
+        end
+        traverse(@current)
+        return @result.join(' ')
+    end
 end
+
+
+tree = BinarySearchTree.new
+tree.insert(50)
+tree.insert(25)
+tree.insert(75)
+tree.insert(15)
+tree.insert(21)
+tree.insert(62)
+tree.insert(91)
+# tree.insert(4)
+# tree.insert(7)
+# tree.insert(13)
+
+print tree.pre_order
+# puts tree.pre_order(tree)
+# # => "8 3 1 6 4 7 10 14 13"
+
+#[5, 3, 7, 1, 2, 6, 8]
+#=>"5 3 1 2 7 6 8"
+
+#[50, 25, 75, 15, 21, 62, 91]
+#=>"50 25 15 21 75 62 91"
