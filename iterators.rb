40bf075b25dba7e280d09dde86f7b689a851b549
Victor Johnson <johnsonsirv@gmail.com>
2019-05-26 15:03:17 +0200

    calc string frequency using iterator

---
+++ iterators.rb
@@ -0,0 +1,18 @@
+def repeat(times)
+    1.upto(times) do
+      yield
+    end
+  end
+  repeat(3){puts "Hello World!"}
+
+  #Let's create a program that will count the frequency of letters (number of occurrences) in a given string. 
+  def string_frequency(string, hash)
+      string.each_char do |char|
+        hash[char]+=1
+      end
+      return hash
+  end
+  hash = Hash.new(0)
+  freqs = string_frequency("I am learning Ruby and it is fun!",hash)
+  ("a".."z").each{|alphab| puts "#{alphab} : #{freqs[alphab]}"}
+  
