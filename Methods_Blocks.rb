efefb01e74015ab275217bf80b23650e1670d5c4
Victor Johnson <johnsonsirv@gmail.com>
2019-05-27 17:49:02 +0200

    positional, options hash, keyword args for methods

--- Methods_Blocks.rb
+++ Methods_Blocks.rb
@@ -1,5 +1,15 @@
 # Working with methods
-def is_dynamic *p
-    print p
+
+#d dynamic optional params =>array
+def is_dynamic (*p)
+     p
 end
 p is_dynamic(2, 4, "i")
+
+#positional arguments
+
+
+#options hash
+
+
+#keyword arguments
