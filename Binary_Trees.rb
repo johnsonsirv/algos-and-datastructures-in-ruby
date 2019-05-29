7363afc90b33c8a5a08f6f094111e7fbddebbaba
Victor Johnson <johnsonsirv@gmail.com>
2019-05-29 17:15:29 +0200

    added method to calc binary tree height recursively - longest depth to last node

--- Binary_Trees.rb
+++ Binary_Trees.rb
@@ -1,21 +1,21 @@
 class Node
-    attr_reader :data
-    attr_accessor :left, :right
-  
-    def initialize(data)
-      @data = data
-    end
+  attr_reader :data
+  attr_accessor :left, :right
+
+  def initialize(data)
+    @data = data
   end
+end
   
-  def array_to_tree(array, i)
-    return nil if i >= array.length || array[i] == 0
-  
-    node = Node.new(array[i])
-    node.left = array_to_tree(array, 2*i+1)
-    node.right = array_to_tree(array, 2*i+2)
-  
-    node
-  end
+def array_to_tree(array, i)
+  return nil if i >= array.length || array[i] == 0
+
+  node = Node.new(array[i])
+  node.left = array_to_tree(array, 2*i+1)
+  node.right = array_to_tree(array, 2*i+2)
+
+  node
+end
   
   def pre_order(node)
     return '' if node.nil?
@@ -32,7 +32,27 @@ class Node
     result += "#{node.data} "
   end
   
-  tree = array_to_tree([10, 1, 2, 3, 4, 5, 6], 0)
-  puts post_order(tree)
+# Binary Tree Height
+def binary_tree_height(node)
+  return 0 if node.nil?
+  left = binary_tree_height(node.left)
+  right = binary_tree_height(node.right)
+  left > right ? left+1 : right+1
+end
+
+
+  # tree = array_to_tree([10, 1, 2, 3, 4, 5, 6], 0)
+  tree = array_to_tree([2, 7, 5, 2, 6, 0, 9], 0)
+  # puts post_order(tree)
   #=> 3 4 1 5 6 2 10
+
+  puts binary_tree_height(tree)
+# => 3
+# [1, 7, 5, 2, 6, 0, 9, 3, 7, 5, 11, 0, 0, 4, 0] =>4
+# [5, 3, 2, 9, 0, 0, 7, 0, 0, 0, 0, 0, 0, 5, 0] => 4
+# [1, 2, 3, 4, 0, 5, 6, 7, 8, 0, 0, 9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 0, 0, 0, 0, 0, 0]=>5
+
+
+  
+
   
