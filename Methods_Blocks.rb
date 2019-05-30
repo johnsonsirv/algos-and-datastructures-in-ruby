6e2e2a52047abaa8f3c7e579d0b98d90192a2bef
Victor Johnson <johnsonsirv@gmail.com>
2019-05-30 16:50:27 +0200

    used keyword args for class initialization

--- Methods_Blocks.rb
+++ Methods_Blocks.rb
@@ -35,3 +35,14 @@ foo(bar: 'baz with block') # => 'baz'
 
 print ["jamie", "jax", "holliday", "today", "zapier"].sort
 print ["jamie", "jax", "holliday", "today", "zapier"].sort{|first, second|  first.length - second.length}
+
+class Bicycle
+  atrr_accessor :make, :color, :frame_size, :weight
+  def initialize
+    @make = make
+    @color = color
+    frame_size = frame_size
+    weight = weight 
+  end
+end
+Bicycle.new(make:'Canyon', color: 'black', frame_size: '178cm', weight: '7.8kg')
