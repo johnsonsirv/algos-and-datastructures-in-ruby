6e059d9233885bcc4d8581a51bed4ae0f94742f0
Victor Johnson <johnsonsirv@gmail.com>
2019-06-03 22:09:29 +0200

    working with classes

---
+++ Classes.rb
@@ -0,0 +1,27 @@
+class Person
+  #setting attributes (set)
+  def name=(str)
+    @name = str
+  end
+  def age=(num)
+    @age = num
+  end
+
+  #getting attributes (get)
+  def name
+    @name
+  end
+  def age
+    @age
+  end
+end
+
+p = Person.new
+p.name = "John Doe"
+p.age = 40
+puts p.name
+
+p2 = Person.new
+p2.name = "Harry Jones"
+p2.age = 23
+puts p2.name
