105d86245adce715b84622b2f352207a7f57b3d9
Victor Johnson <johnsonsirv@gmail.com>
2019-06-03 22:40:45 +0200

    using constructors to set attr during instatiation

--- Classes.rb
+++ Classes.rb
@@ -17,14 +17,24 @@ class Person
   # def age
   #   @age
   # end
+
+  # using constructor to set attr when we instantiate obj
+  def initialize(obj_name, obj_age)
+    @name = obj_name
+    @age = obj_age
+  end
 end
 
-p = Person.new
-p.name = "John Doe"
-p.age = 40
-puts p.name
+# p = Person.new
+# p.name = "John Doe"
+# p.age = 40
+# puts p.name
 
-p2 = Person.new
-p2.name = "Harry Jones"
-p2.age = 23
-puts p2.name
+# p2 = Person.new
+# p2.name = "Harry Jones"
+# p2.age = 23
+# puts p2.name
+
+p3 = Person.new("Kelly Done", 50)
+puts p3.name
+puts p3.age
