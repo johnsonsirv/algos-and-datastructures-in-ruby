de8ab733bc93aa1305d3b6b1fd252922fc5437b5
Victor Johnson <johnsonsirv@gmail.com>
2019-06-05 18:13:37 +0200

    using reflection in lamdas

--- Blocks_proc_lambdas.rb
+++ Blocks_proc_lambdas.rb
@@ -10,3 +10,21 @@ numbers_array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 strings_array = numbers_array.map(&:to_s)
  
 puts strings_array
+
+# Lambdas
+strings = ["leonardo", "donatello", "raphael", "michaelangelo"]
+# Write your code below this line!
+
+symbolize = lambda{ |str| str.to_sym}
+
+# Write your code above this line!
+symbols = strings.collect(&symbolize)
+print symbols
+
+my_array = ["raindrops", :kettles, "whiskers", :mittens, :packages]
+
+# Add your code below!
+symbol_filter = lambda{ |val| val.is_a? Symbol }
+symbols = my_array.select(&symbol_filter)
+
+puts symbols
