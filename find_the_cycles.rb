cf75a6f0c874a7d392610d9f6b7a71d935d7a2c8
Victor Johnson <johnsonsirv@gmail.com>
2019-06-05 15:59:46 +0200

    added code logic to method

--- find_the_cycles.rb
+++ find_the_cycles.rb
@@ -3,14 +3,25 @@
 
 def graph_cycle?(graph)
   # write your code here
-  
+  visited = Hash.new
+  return true if has_cycle_by_DFS?(dfs_graph: graph, dfs_visited: visited, dfs_vertex: 0, parent_vertex: -1)
+  false
 end
 
-def has_cycle_by_DFS?(dfs_graph: graph, dfs_visited: visited, current_vertex: vertex, parent_vertex: parent)
-  
+def has_cycle_by_DFS?(dfs_graph: graph, dfs_visited: visited, dfs_vertex: vertex, parent_vertex: parent)
+  return false if dfs_graph[dfs_vertex].nil?
+  dfs_visited[dfs_vertex] = true
+  dfs_graph[dfs_vertex].each do |neighbour|
+    unless dfs_visited[neighbour]
+      return true if has_cycle_by_DFS?(dfs_graph: dfs_graph, dfs_visited: dfs_visited, dfs_vertex: neighbour, parent_vertex: dfs_vertex)
+    else
+      return true unless neighbour.eql? parent_vertex
+    end
+  end
+  false # false no back-edge was found
 end
 
-puts graph_cycle?({
+p graph_cycle?({
   0=>[2], 
   1=>[4], 
   2=>[0, 5, 3], 
@@ -20,7 +31,7 @@ puts graph_cycle?({
 })
 # => true
 
-puts graph_cycle?({
+p graph_cycle?({
   0=>[2], 
   1=>[2], 
   2=>[0, 1, 3, 4, 5], 
