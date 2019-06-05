4d5bfefa01de61bb6156a56b231a3365ccaefa58
Victor Johnson <johnsonsirv@gmail.com>
2019-06-05 18:34:15 +0200

    filter hash using lamda

--- Blocks_proc_lambdas.rb
+++ Blocks_proc_lambdas.rb
@@ -28,3 +28,18 @@ symbol_filter = lambda{ |val| val.is_a? Symbol }
 symbols = my_array.select(&symbol_filter)
 
 puts symbols
+
+crew = {
+  captain: "Picard",
+  first_officer: "Riker",
+  lt_cdr: "Data",
+  lt: "Worf",
+  ensign: "Ro",
+  counselor: "Troi",
+  chief_engineer: "LaForge",
+  doctor: "Crusher"
+}
+# Add your code below!
+first_half = lambda{ |key, value| value < "M" }
+a_to_m = crew.select(&first_half)
+puts a_to_m
