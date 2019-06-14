adf3b87697c6bba7ca7186e1b0f5c613ffb7a6ba
Victor Johnson <johnsonsirv@gmail.com>
2019-06-14 13:42:59 +0200

    code logic to find prime nos

--- Erathosthenes_prime_numbers.rb
+++ Erathosthenes_prime_numbers.rb
@@ -3,16 +3,56 @@ require 'set'
 # seive prime numbers from 2 to n
 def erathosthenes_seive(n)
     list = []
-    (2..n).upto(n) { |i|   list[i] << true }
+    2.upto(n) { |i|  list[i] = true  }
 
+    2.upto(Math.sqrt(n).floor) do |index|
+      if list[index]
+        multiplier = 0
+        multiple = (index ** 2) + multiplier
+        while multiple <= n
+          list[multiple] = false
+          multiplier += 1
+          multiple = (index ** 2) + (multiplier * index)
+        end
+      end
+    end
+    primes = []
+    list.each_with_index { |v, index| primes << index if list[index] }
+    primes
 end
+
+def primes(max_number)
+  integers = []
+  i = 2
+  while i <= max_number
+    integers << i
+    i = i + 1
+  end
+  index = 0 
+  multiplier = 2
+  while integers[index] != nil
+    while (integers[index] * multiplier) <= max_number
+      integers.delete(integers[index] * multiplier)
+      multiplier = multiplier + 1
+    end
+    index = index + 1
+    multiplier = 2 
+  end 
+  integers
+end
+
 
 def number_of_primes(arr)
   
 end
 
-puts number_of_primes([121, 17, 21, 29, 11, 341, 407, 19, 119, 352])
+p erathosthenes_seive(5)
+
+p primes(5)
+
+# puts number_of_primes([121, 17, 21, 29, 11, 341, 407, 19, 119, 352])
 # => 4
 
-puts number_of_primes([7, 6, 7, 3, 77, 14, 28, 35, 42])
+# puts number_of_primes([7, 6, 7, 3, 77, 14, 28, 35, 42])
 # => 3
+
