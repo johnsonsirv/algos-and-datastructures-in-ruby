850d3bf34db2b5bde83a83fd933ae0a3c0d9f76b
Victor Johnson <johnsonsirv@gmail.com>
2019-06-15 15:01:24 +0200

    private, public methods, mixins

--- Classes.rb
+++ Classes.rb
@@ -80,4 +80,40 @@ child_obj.parent_method
 p child_obj.name
 child_obj.sub_method
 child_obj.sub_private_method
+class Account
+  attr_reader :name, :balance
+  
+  def initialize(name, balance = 100)
+    @name = name
+    @balance = balance
+  end
+  
+  def display_balance(pin_number)
+    if pin_number.eql? pin
+      puts "Balance: $#{@balance}."
+    else
+      pin_error
+    end
+  end
+  
+  def withdraw(pin_number, amount)
+    if pin_number.eql? pin
+      @balance -= amount
+      puts "Withdrew #{amount}. New balance: $#{@balance}."
+    else
+      pin_error
+    end
+  end
+  
+  private
+  def pin
+    @pin = 1234
+  end
+  
+  def pin_error
+    "Access denied: incorrect PIN."
+  end
+end
+
+checking_account = Account.new("jerry", 5000)
 
