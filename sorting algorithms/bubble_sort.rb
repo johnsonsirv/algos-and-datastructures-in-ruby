2e57f1b969f2494cb907a4d3b5eef7c60f9a94ad
Victor Johnson <johnsonsirv@gmail.com>
2019-06-20 03:03:00 +0200

    added insertion sort method, to sort and display sort order

---
+++ sorting algorithms/bubble_sort.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+# bubble sort algorithm
+def bubble_sort(arr)
+  upper_bound = arr.length - 1
+  upper_bound.downto(0) do |count|
+    swapped = false
+    (0...count).each do |indx|
+      if arr[indx] > arr[indx + 1]
+        arr[indx], arr[indx + 1] = arr[indx + 1], arr[indx]
+        swapped = true
+      end
+    end
+    break unless swapped
+  end
+  arr
+end
+
+# bubble_sort by
+def bubble_sort_by(arr)
+  upper_bound = arr.length - 1
+  upper_bound.downto(0) do |count|
+    swapped = false
+    (0...count).each do |indx|
+      if yield(arr[indx], arr[indx + 1]).positive?
+        arr[indx], arr[indx + 1] = arr[indx + 1], arr[indx]
+        swapped = true
+      end
+    end
+    break unless swapped
+  end
+  arr
+end
--- sorting algorithms/insertion_sort.rb
+++ sorting algorithms/insertion_sort.rb
@@ -1,13 +1,64 @@
 
 
 def insertion_sort(arr)
-  
-  1.upto(arr.size) do |indx|
-    arr_end = arr.size - 1
+  1.upto(arr.size - 1) do |indx|
     current = arr[indx]
-    arr_end.upto(0) do |ind|
-      
+    j = indx -1
+    while j >= 0 && arr[j] > current
+      arr[j + 1] = arr[j]
+      j -= 1
     end
+    arr[j + 1] = current
+  end
+  arr
+end
+
+def insertion_sort_order(array)
+  sorting_order = []
+  sorted = nil
+  1.upto(array.size - 1) do |indx|
+    current = array[indx]
+    backward_indx = indx - 1
+    while backward_indx >= 0 && array[backward_indx] > current
+      array[backward_indx + 1] = array[backward_indx]
+      backward_indx -= 1
+      sorting_order << array.join(' ')
+    end
+    array[backward_indx + 1] = current
+    sorted =  array
+  end
+  sorting_order << sorted.join(' ')
+  
+  puts sorting_order
+end
+
+def sort_itself(array)
+  1.upto(array.size - 1) do |indx|
+    current = array[indx]
+    j = indx -1
+    while j >= 0 && array[j] > current
+      array[j + 1] = array[j]
+      j -= 1
+    end
+    array[j + 1] = current
+    puts arr.join(' ')
   end
   
 end
+
+p insertion_sort([1, 3, 2])
+
+insertion_sort_order([1, 3, 2])
+
+# insertion_sort_order([1, 4, 6, 9, 3])
+# => 1 4 6 9 9
+#    1 4 6 6 9
+#    1 4 4 6 9
+#    1 3 4 6 9
+
+sort_itself([1, 4, 3, 6, 9, 2])
+# => 1 4 3 6 9 2
+#    1 3 4 6 9 2
+#    1 3 4 6 9 2
+#    1 3 4 6 9 2
+#    1 2 3 4 6 9
