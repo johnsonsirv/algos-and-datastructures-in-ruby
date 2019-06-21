11b060b904c91b8a612f9a613eddacbc9988e57e
Victor Johnson <johnsonsirv@gmail.com>
2019-06-21 19:27:31 +0200

    fixed bug with set

--- Hash_Tables.rb
+++ Hash_Tables.rb
@@ -1,16 +1,16 @@
 class HashMap
   attr_accessor :key_map
   def initialize(size)
-    @key_map = size
+    @key_map = Array.new(size)
   end
 
   def set(key, value)
     index = hash_function(key)
-    unless @key_map[key]
-      @key_map = []
+    unless @key_map[index]
+      @key_map[index] = []
     end
-    
-    @key_map << [key, value]
+
+    @key_map[index] << [key, value]
   end
 
   def get(key)
@@ -25,10 +25,11 @@ end
 
 def hash_table(array)
   ht = HashMap.new(array.size)
+  array.each{ |elem| ht.set(elem, elem)}
 
 end
 p hash_table([12, 24, 125, 5, 91, 1106, 2, 1021, 29, 3536, 10])
 # => [12, 24, 2, 91, 125, 5, 3536, 1106, 29, 1021, 10]
 
-p hash_table([2, 341, 73, 8265, 234004, 602, 7400000, 200000000])
+# p hash_table([2, 341, 73, 8265, 234004, 602, 7400000, 200000000])
 # => [341, 234004, 2, 200000000, 7400000, 8265, 73, 602]
