d2d6846e1fb9516b16582bfa14e67cb896100625
Victor Johnson <johnsonsirv@gmail.com>
2019-06-22 16:21:24 +0200

    array.combinations not okay for order data in some implementations

--- pairs_to_sum.rb
+++ pairs_to_sum.rb
@@ -13,10 +13,12 @@ def find_pairs(array, k)
   set_pairs.map { |pair| pair.to_a}
 end
 
-p find_pairs([1, 9, 11, 13, 2, 3, 7], 12)
+# p find_pairs([1, 9, 11, 13, 2, 3, 7], 12)
 # => [[1, 11], [9, 3]]
-# moves = [1,3,4,5]
-# win = [ [0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6] ]
-# comb = moves.combination(3).to_a
-# p comb
-# p comb & win
+moves = [4, 5, 1, 7]
+win = [ [0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6] ]
+comb = moves.combination(3).to_a
+p comb
+p comb & win
+p [moves] & win
+
