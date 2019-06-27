9646502a4e6b3592e6ce6df71087d557660c449c
Victor Johnson <johnsonsirv@gmail.com>
2019-06-27 17:02:37 +0200

    fixed bug with out of bounds positions

--- explorer_maze_escape.rb
+++ explorer_maze_escape.rb
@@ -1,29 +1,32 @@
 
 
-def mark_position(xy)
-  return [x, y]
+def out_of_bound?(x, y, maze)
+  n = maze[0].size - 1
+  return false if (0..n).cover?(x) && (0..n).cover?(y)
+  true
 end
 
 def find_path(x,y, maze, goal, blocked)
   current_position = maze[x][y]
+  p "#{current_position}, x -> #{x}, y -> #{y}"
   #base case
-  return false if current_position.nil?
-  return true if current_position == goal
-  return false if current_position == blocked
-  case current_position
-  when nil?
+  # return false if current_position.nil?
+  # return true if current_position == goal
+  # return false if current_position == blocked
+  
+  if out_of_bound?(x, y, maze)
     return false
-  when goal
+  elsif current_position == goal
     return true
-  when blocked
+  elsif current_position == blocked
     return false
   else
     #mark position
     $solution_path << [x, y]
     return true if find_path(x, y-1, maze, goal, blocked) # north
     return true if find_path(x + 1, y, maze, goal, blocked) # east
-    return true if find_path(x, y + 1, maze, goal,  blocked) # south
-    return true if find_path(x - 1, y, maze, goal, blocked) # west
+    # return true if find_path(x, y + 1, maze, goal,  blocked) # south
+    # return true if find_path(x - 1, y, maze, goal, blocked) # west
     #unmark position
     $solution_path.pop
   end
@@ -46,5 +49,7 @@ maze =  [
   [0, 0, 0, 1, 9]
 ]
 
-# puts maze_escape(maze)
-p maze[0][-1] === nil
+p maze_escape(maze)
+x = -1
+y = 1
+# p out_of_bound?(x, y, maze)
