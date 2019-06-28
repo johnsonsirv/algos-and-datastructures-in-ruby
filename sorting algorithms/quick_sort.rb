18a7799db55e9a8772802a8b1fd6d58ae01626cf
Victor Johnson <johnsonsirv@gmail.com>
2019-06-28 10:41:55 +0200

    recursive quicksort

--- sorting algorithms/quick_sort.rb
+++ sorting algorithms/quick_sort.rb
@@ -1,5 +1,5 @@
 
-def pivot(array, start_index = 0, end_index = array.size - 1)
+def pivot(array, start_index = 0, end_index = array.size + 1)
   pivot = array[start_index]
   swap_index = start_index
   start_loop = start_index + 1
@@ -8,13 +8,25 @@ def pivot(array, start_index = 0, end_index = array.size - 1)
       swap_index += 1
       array[swap_index], array[idx] = array[idx], array[swap_index]
     end
-    p array
+    # p array
   end
   array[start_index], array[swap_index] = array[swap_index], array[start_index]
   
-  p array
+  # p array
+  swap_index
 end
 
-# pivot([4,8,2,1,5,7,6,3])
-pivot([4, 5, 3, 9, 1])
+def quicksort(array, left = 0, right = array.length - 1)
+  if left < right
+    pivot_indx = pivot(array, left, right)
+    # left
+    quicksort(array, left, pivot_indx - 1)
+    # right
+    quicksort(array, pivot_indx + 1, right)
+  end
+  array
+end
+
+p quicksort([4,8,2,1,5,7,6,3])
+# pivot([4, 5, 3, 9, 1])
 # => [3, 1, 4, 5, 9]
