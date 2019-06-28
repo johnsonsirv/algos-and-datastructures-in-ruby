29e799eea3d726e89179017d93cb2d0859413909
Victor Johnson <johnsonsirv@gmail.com>
2019-06-28 18:46:49 +0200

    refactored and improved speed by 70%

--- connected_components.rb
+++ connected_components.rb
@@ -1,5 +1,5 @@
 
-
+require 'benchmark/ips'
 require 'set'
 def graph_DFS_recursive(graph: hash_graph)
     $result= []
@@ -18,39 +18,78 @@ end
 
 
 def connected_graph?(graph)
-  dfs = graph_DFS_recursive(graph: graph)
-  hash = graph.values.flatten.uniq
-  dfs.to_set == hash.to_set
+  # dfs = graph_DFS_recursive(graph: graph)
+  # hash = graph.values.flatten.uniq
+  # dfs.to_set == hash.to_set
+  graph.keys.size == graph_DFS_recursive(graph: graph).size
 end
 
-puts connected_graph?({
+
+def depth_first_search_model(graph)
+  # write your code here
+  result = []
+  stack = [0]
+  visited = []
+  
+  while stack.size != 0
+    head = stack.pop
+    result << head
+    visited << head
+    
+    not_visited = graph[head].reject { |node| visited.include? node }
+    
+    visited += not_visited
+    stack += not_visited.reverse
+  end
+  
+  result
+end
+
+def connected_graph_model_solution?(graph)
+  # write your code here
+  graph.keys.size == depth_first_search_model(graph).size
+end
+
+# puts connected_graph?({
+#   0 => [2], 
+#   1 => [4], 
+#   2 => [0, 5, 3], 
+#   3 => [5, 2], 
+#   4 => [5, 1], 
+#   5 => [4, 2, 3]
+# })
+# => true
+
+# puts connected_graph?({
+#   0 => [1, 2], 
+#   1 => [0, 2], 
+#   2 => [0, 1, 3, 4, 5], 
+#   3 => [2, 4], 
+#   4 => [3, 2], 
+#   5 => [2]
+# })
+# => true
+
+# puts connected_graph?({
+#   0=>[1], 
+#   1=>[0, 3, 4], 
+#   2=>[5, 6], 
+#   3=>[1], 
+#   4=>[1], 
+#   5=>[2], 
+#   6=>[2]
+# })
+# => false
+ graph = {
   0 => [2], 
   1 => [4], 
   2 => [0, 5, 3], 
   3 => [5, 2], 
   4 => [5, 1], 
   5 => [4, 2, 3]
-})
-# => true
-
-puts connected_graph?({
-  0 => [1, 2], 
-  1 => [0, 2], 
-  2 => [0, 1, 3, 4, 5], 
-  3 => [2, 4], 
-  4 => [3, 2], 
-  5 => [2]
-})
-# => true
-
-puts connected_graph?({
-  0=>[1], 
-  1=>[0, 3, 4], 
-  2=>[5, 6], 
-  3=>[1], 
-  4=>[1], 
-  5=>[2], 
-  6=>[2]
-})
-# => false
- 
+}
+Benchmark.ips do |x|
+  x.report("model_solution") { connected_graph_model_solution?(graph) }
+  x.report("victor_solution") { connected_graph?(graph) }
+  x.compare!
+end
