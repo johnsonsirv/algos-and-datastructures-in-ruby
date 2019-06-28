dc8d238adef44ffb212f39523cbe24ae0efb989b
Victor Johnson <johnsonsirv@gmail.com>
2019-06-28 20:02:59 +0200

    updated backtracking recursion; bechmarked code performance

--- Backtracing_recursion.rb
+++ Backtracing_recursion.rb
@@ -1,6 +1,7 @@
 =begin
     Given a number k and a list of coins, determine if the coins can be added together to equal k. Print true if you can get the exact sum and false otherwise.
 =end
+require 'benchmark/ips'
 def exact_sum?(k, coins)
     #write your code here
     if coins.length == 0
@@ -30,9 +31,26 @@ def exact_sum?(k, coins)
         find_sum(sum, coins.tap(&:pop))
       end
   end
-  print find_compliments(201, [37, 42, 10, 23, 15, 25 11, 6, 19, 25, 51])
+
+  def exact_sum_two?(k, coins)
+    # print "#{k}, #{coins} \n"
+    return true if k == 0
+    return false if coins.empty? || k.negative?
+    exact_sum_two?(k-coins[coins.length-1], coins[0...coins.length-1]) ||  exact_sum_two?(k, coins[0...coins.length-1]) 
+  end
+
+  def exact_sum_model_solution?(k, coins)
+    # print "#{k}, #{coins} \n"
+    return true if k == 0
+    return false if coins.empty? || k.negative?
+    exact_sum_model_solution?(k-coins[0], coins[1..-1]) || exact_sum_model_solution?(k, coins[1..-1]) 
+  end
+  # print find_compliments(201, [37, 42, 10, 23, 15, 25, 11, 6, 19, 25, 51])
 #   print find_sum(0,[37, 42, 10, 23, 15, 25, 11, 6, 19, 25, 51])
   # puts exact_sum?(12, [1, 2, 3, 4, 5])
+  # => true
+
+  # puts exact_sum_two?(12, [1, 2, 3, 4, 5])
   # => true
   
   # puts exact_sum?(11, [1, 5, 9, 13])
@@ -42,3 +60,8 @@ def exact_sum?(k, coins)
 #   compliments =       [164, 159, 191, 178, 176, 190, 195, 176, 150]
   # =>true
   
+  Benchmark.ips do |x|
+    x.report("victor_solution") {  exact_sum_two?(12, [1, 2, 3, 4, 5]) }
+    x.report("model_solution") {  exact_sum_model_solution?(12, [1, 2, 3, 4, 5]) }
+    x.compare!
+  end
