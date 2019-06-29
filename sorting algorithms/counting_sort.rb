e194bccbe7fa493fa37b9fe26350eda33ec957ea
Victor Johnson <johnsonsirv@gmail.com>
2019-06-29 17:17:35 +0200

    refactored to use array.reverse_each on origin array

--- sorting algorithms/counting_sort.rb
+++ sorting algorithms/counting_sort.rb
@@ -63,24 +63,17 @@ end
 def full_counting_sort(array)
   # write your code here
   integers = array.map {|elem| elem.split.first.to_i}
-  str_values = array.map {|elem| elem.split.last}
   counts = counting_sort_with_array(integers)
   prefix_sum = prefix_sum(counts)
-  sorted_array = Array.new(str_values.size , 0)
-    # p counts
-    # p prefix_sum
-    # p integers
-    # p str_values
-  array.each do |elem|
+  sorted_array = Array.new(str_values.size , nil)
+  array.reverse_each do |elem|
     idx = elem.split.first.to_i
     val = elem.split.last
-    # print idx
-    # p val
-    sorted_array[prefix_sum[idx]] = elem
+    sorted_array[prefix_sum[idx]] = val
     prefix_sum[idx] -= 1
   end
 
-  sorted_array
+  sorted_array.compact
 end
 
 p full_counting_sort(["0 ab", "6 cd", "0 ef", "6 gh", "4 ij", "0 ab", "6 cd", "0 ef", "6 gh", "0 ij", "4 that", "3 be", "0 to", "1 be", "5 question", "1 or", "2 not", "4 is", "2 to", "4 the"])
@@ -89,7 +82,7 @@ p full_counting_sort(["0 ab", "6 cd", "0 ef", "6 gh", "4 ij", "0 ab", "6 cd", "0
 # p full_counting_sort_prep(["4 that", "3 be", "0 to", "1 be", "5 question", "1 or", "2 not", "4 is", "2 to", "4 the"])
 # => [1, 3, 5, 6, 9, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10]
 
-p simple_counting_sort( [0, 6, 0, 6, 4, 0, 6, 0, 6, 0, 4, 3, 0, 1, 5, 1, 2, 4, 2, 4])
+# p simple_counting_sort( [0, 6, 0, 6, 4, 0, 6, 0, 6, 0, 4, 3, 0, 1, 5, 1, 2, 4, 2, 4])
 # p simple_counting_sort([63, 25, 73, 1, 98, 73, 56, 84, 86, 57, 16, 83, 8, 25, 81, 56, 9, 53, 98, 67, 99, 12, 83, 89, 80, 91, 39, 86, 76, 85, 74, 39, 25, 90, 59, 10, 94, 32, 44, 3, 89, 30, 27, 79, 46, 96, 27, 32, 18, 21, 92, 69, 81, 40, 40, 34, 68, 78, 24, 87, 42, 69, 23, 41, 78, 22, 6, 90, 99, 89, 50, 30, 20, 1, 43, 3, 70, 95, 33, 46, 44, 9, 69, 48, 33, 60, 65, 16, 82, 67, 61, 32, 21, 79, 75, 75, 13, 87, 70, 33])
 # => [1, 1, 3, 3, 6, 8, 9, 9, 10, 12, 13, 16, 16, 18, 20, 21, 21, 22, 23, 24, 25, 25, 25, 27, 27, 30, 30, 32, 32, 32, 33, 33, 33, 34, 39, 39, 40, 
 # 40, 41, 42, 43, 44, 44, 46, 46, 48, 50, 53, 56, 56, 57, 59, 60, 61, 63, 65, 67, 67, 68, 69, 69, 69, 70, 70, 73, 73, 74, 75, 75, 76, 78, 78, 79, 79, 80,
