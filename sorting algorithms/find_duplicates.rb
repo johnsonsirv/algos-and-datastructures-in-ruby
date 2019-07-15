0672a240927a3c7cf18cf0ce379745a1c988951b
Victor Johnson <johnsonsirv@gmail.com>
2019-07-15 23:26:42 +0200

    :gs

---
+++ sorting algorithms/find_duplicates.rb
@@ -0,0 +1,8 @@
+#Find all the 'extra' numbers that are in B but not in A, and output them in ascending order.
+def duplicates(arr1, arr2)
+  # write your code here
+    arr1.each {|n|
+    arr2.delete_at(arr2.index(n))
+  }
+  arr2.sort
+end
