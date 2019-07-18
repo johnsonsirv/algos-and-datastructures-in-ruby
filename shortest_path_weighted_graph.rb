82bee9af3029d6809e8db98bfeadb2b203d8bd1f
Victor Johnson <johnsonsirv@gmail.com>
2019-07-18 15:43:32 +0200

    get count of station stop, and update the min_station_stops

--- shortest_path_weighted_graph.rb
+++ shortest_path_weighted_graph.rb
@@ -1,21 +1,23 @@
 def weighted_graphs(matrix)
   # write your code here
   weighted_hash_graph = adjacency_list(matrix)
-  station_stops = Array.new(matrix.size, 0)
+  min_station_stops = Array.new(matrix.size, 0)
   start_node = 0
-  queue = []
+  queue = [start_node]
   visited = [start_node]
 
   until queue.empty?
     current_node = queue.shift
-    weighted_hash_graph[current_node].each do|value|
-      unless visited.include?(value)
-          visited << value
-          queue << value
+    neighbours = weighted_hash_graph[current_node]
+    neighbours.each do|vertex|
+      unless visited.include?(vertex)
+          visited << vertex
+          queue << vertex
+          min_station_stops[vertex] += get_count_station_stops(neighbours, start_position)
       end
     end
   end
-
+  min_station_stops
 end
 
 def adjacency_list(matrix)
@@ -29,14 +31,11 @@ def adjacency_list(matrix)
   hash_graph
 end
 
-def update_station_stops(arr_connected_nodes, node, start_position)
-  count = 0
-  if arr_connected_nodes[node].include?(start_position)
-    count += 1
-  else
-    count += 2
+def get_count_station_stops(arr_connected_nodes, start_position)
+  unless arr_connected_nodes.include?(start_position)
+    return 2
   end
-  count
+  return 1
 end
 
 p weighted_graphs([[0, 0, 1, 3, 0, 0], [0, 0, 0, 5, 0, 0], [1, 0, 0, 2, 1, 4], [3, 5, 2, 0, 7, 0], [0, 0, 1, 7, 0, 2], [0, 0, 4, 0, 2, 0]])
