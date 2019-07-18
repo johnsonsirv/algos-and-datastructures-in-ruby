e28b1bee5c5e25cdb3f9c36f737ff15c8bfb82e5
Victor Johnson <johnsonsirv@gmail.com>
2019-07-18 14:40:49 +0200

    added method to update the minimum station stops during traversal

--- shortest_path_weighted_graph.rb
+++ shortest_path_weighted_graph.rb
@@ -1,6 +1,7 @@
 def weighted_graphs(matrix)
   # write your code here
   weighted_hash_graph = adjacency_list(matrix)
+
 end
 
 def adjacency_list(matrix)
@@ -12,6 +13,10 @@ def adjacency_list(matrix)
     end
   end
   hash_graph
+end
+
+def update_station_stops(arr_station_stops, arr_connected_nodes, node )
+  
 end
 
 p weighted_graphs([[0, 0, 1, 3, 0, 0], [0, 0, 0, 5, 0, 0], [1, 0, 0, 2, 1, 4], [3, 5, 2, 0, 7, 0], [0, 0, 1, 7, 0, 2], [0, 0, 4, 0, 2, 0]])
