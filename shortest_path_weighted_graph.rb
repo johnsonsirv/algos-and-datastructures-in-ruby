14196c78f06d94dd0beb7c6e7ae639f1fb58587b
Victor Johnson <johnsonsirv@gmail.com>
2019-07-18 14:11:19 +0200

    added method to return matrix as adjacency list

--- shortest_path_weighted_graph.rb
+++ shortest_path_weighted_graph.rb
@@ -3,5 +3,9 @@ def weighted_graphs(matrix)
   
 end
 
+def adjacency_list(mattrix)
+  
+end
+
 p weighted_graphs([[0, 0, 1, 3, 0, 0], [0, 0, 0, 5, 0, 0], [1, 0, 0, 2, 1, 4], [3, 5, 2, 0, 7, 0], [0, 0, 1, 7, 0, 2], [0, 0, 4, 0, 2, 0]])
 # => [0, 2, 1, 1, 2, 2]
