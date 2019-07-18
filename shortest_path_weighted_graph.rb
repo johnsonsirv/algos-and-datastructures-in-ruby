9f207a71bb6386027e1df455f57aabab3e71fa14
Victor Johnson <johnsonsirv@gmail.com>
2019-07-18 11:45:00 +0200

    initial commit

---
+++ shortest_path_weighted_graph.rb
@@ -0,0 +1,7 @@
+def weighted_graphs(matrix)
+  # write your code here
+  
+end
+
+p weighted_graphs([[0, 0, 1, 3, 0, 0], [0, 0, 0, 5, 0, 0], [1, 0, 0, 2, 1, 4], [3, 5, 2, 0, 7, 0], [0, 0, 1, 7, 0, 2], [0, 0, 4, 0, 2, 0]])
+# => [0, 2, 1, 1, 2, 2]
