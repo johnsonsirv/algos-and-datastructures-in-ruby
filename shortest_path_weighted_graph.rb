fecd4ae6b63a6daac1ed2efb962ff3f5987a9678
Victor Johnson <johnsonsirv@gmail.com>
2019-07-18 15:24:36 +0200

    bfs on neighbours and enqueue, then add to visited

--- shortest_path_weighted_graph.rb
+++ shortest_path_weighted_graph.rb
@@ -7,7 +7,13 @@ def weighted_graphs(matrix)
   visited = [start_node]
 
   until queue.empty?
-    
+    current_node = queue.shift
+    weighted_hash_graph[current_node].each do|value|
+      unless visited.include?(value)
+          visited << value
+          queue << value
+      end
+    end
   end
 
 end
@@ -23,8 +29,14 @@ def adjacency_list(matrix)
   hash_graph
 end
 
-def update_station_stops(arr_station_stops, arr_connected_nodes, node, start_position)
-  
+def update_station_stops(arr_connected_nodes, node, start_position)
+  count = 0
+  if arr_connected_nodes[node].include?(start_position)
+    count += 1
+  else
+    count += 2
+  end
+  count
 end
 
 p weighted_graphs([[0, 0, 1, 3, 0, 0], [0, 0, 0, 5, 0, 0], [1, 0, 0, 2, 1, 4], [3, 5, 2, 0, 7, 0], [0, 0, 1, 7, 0, 2], [0, 0, 4, 0, 2, 0]])
