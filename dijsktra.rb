dbc705aa3821d583008b10a63a380f638007097f
Victor Johnson <johnsonsirv@gmail.com>
2019-07-21 18:12:22 +0200

    added code logic to enqueue and dequeue Prioirity Queue

--- dijsktra.rb
+++ dijsktra.rb
@@ -10,14 +10,33 @@ def adjacency_list(matrix)
   weighted_hash_graph #key -> node ; value -> weight
 end
 
-def priority_queue(vertex, priority)
+class PriorityQueue
+  def initialize
+    @values = []
+  end
   
+  def enqueue(vertex, priority)
+    @values << {vertex: vertex, priority: priority}
+    @values.sort_by! { |a| a[:priority] }
+
+  end
+
+  def dequeue
+    @values.shift
+  end
 end
 
 def dijkstra(matrix)
   adjacency_list(matrix)
 end
 
+pQ = PriorityQueue.new
 
-p dijkstra([[0, 0, 1, 3, 0, 0], [0, 0, 0, 5, 0, 0], [1, 0, 0, 2, 1, 4], [3, 5, 2, 0, 7, 0], [0, 0, 1, 7, 0, 2], [0, 0, 4, 0, 2, 0]])
+pQ.enqueue("d", 12)
+pQ.enqueue("e", 3)
+pQ.enqueue("b", 1)
+pQ.enqueue("c", 2)
+
+p pQ
+# p dijkstra([[0, 0, 1, 3, 0, 0], [0, 0, 0, 5, 0, 0], [1, 0, 0, 2, 1, 4], [3, 5, 2, 0, 7, 0], [0, 0, 1, 7, 0, 2], [0, 0, 4, 0, 2, 0]])
 # => [0, 2, 1, 1, 2, 2]
