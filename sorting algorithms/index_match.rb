a06f276368b29005afc51c4f139072de7a098cc6
Victor Johnson <johnsonsirv@gmail.com>
2019-07-26 14:30:00 +0200

    fixed bug

--- sorting algorithms/index_match.rb
+++ sorting algorithms/index_match.rb
@@ -5,9 +5,9 @@ def index_match(array)
   max_ind = array.size - 1
   start = 0
   stop = array.size - 1
-  pivot = array.size / 2
+  pivot = (array.size - 1) / 2
   if array[pivot] > max_ind
-    stop = pivot 
+    stop = pivot
   else
     start = pivot
   end
@@ -18,14 +18,16 @@ def index_match(array)
   -1
 end
 
-puts index_match([0, 2, 3, 7, 9, 11])
+# puts index_match([0, 2, 3, 7, 9, 11])
 # => 0
 
-p index_match([3, 5, 7, 9, 11, 13, 17])
+# p index_match([3, 5, 7, 9, 11, 13, 17])
 # -1
 
-p index_match([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+# p index_match([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
 # -1
 
-p  index_match([-4, -2, 0, 2, 4, 6, 8, 10])
+# p  index_match([-4, -2, 0, 2, 4, 6, 8, 10])
 # 4
+p index_match([-25, -20, -15, -10, -5, 5, 7, 8, 9, 10, 11, 12])
+# 5
