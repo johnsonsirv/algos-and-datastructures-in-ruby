c7e916ee5db35f1c699c36058f29fba8151c6cd9
Victor Johnson <johnsonsirv@gmail.com>
2019-07-26 09:40:57 +0200

    rethink span tree flow

--- minimum_spanning_tree.rb
+++ minimum_spanning_tree.rb
@@ -13,24 +13,33 @@ end
 def minimum_spanning_tree(matrix)
   # write your code here
   hash_grpah = adjacency_list(matrix)
-  queue = [0]
-  span_tree = []
+  start_node = 0
+  cost = []
+  edge = []
+  cost[start_node] = Float::INFINITY
   
-  until queue.empty?
-    current_node = queue.shift
-    min_span = Float::INFINITY
-    solved_node = nil
-    hash_grpah[current_node].each do |neighbour|
-      if neighbour[:weight] < min_span
-        min_span = neighbour[:weight]
-        solved_node = neighbour[:node]
-      end
-    end
-    queue << solved_node unless solved_node.nil?
-    span_tree << min_span unless min_span.eql(Float::INFINITY)
-  end
+  
+  
+  # until solved_nodes.empty?
+  #   current_node = queue.shift
+  #   min_span = Float::INFINITY
+  #   solved_node = nil
+  #   # p "cur no #{current_node}, min_spa #{min_span}, solv_no #{solved_node}"
+  #   hash_grpah[current_node].each do |neighbour|
+  #     unless solved_nodes.include?(neighbour[:node])
+  #       if neighbour[:weight] < min_span
+  #         min_span = neighbour[:weight]
+  #         solved_node = neighbour[:node]
+  #       end
+  #     end
+      
+  #   end
+  #   queue << current_node unless solved_node.include?(current_node)
+  #   span_tree << min_span unless min_span.eql?(Float::INFINITY)
+  # end
 
-  span_tree
+  # span_tree
+  hash_grpah
 end
 
 p minimum_spanning_tree([[0, 4, 1, 4, 0, 0, 0, 0, 0, 0], [4, 0, 5, 0, 9, 9, 0, 7, 0, 0], [1, 5, 0, 3, 0, 0, 0, 9, 0, 0], [4, 0, 3, 0, 0, 0, 0, 10, 0, 18], [0, 9, 0, 0, 0, 2, 4, 0, 6, 0], [0, 9, 0, 0, 2, 0, 2, 8, 0, 0], [0, 0, 0, 0, 4, 2, 0, 9, 3, 9], [0, 7, 9, 10, 0, 8, 9, 0, 0, 8], [0, 0, 0, 0, 6, 0, 3, 0, 0, 9], [0, 0, 0, 18, 0, 0, 9, 8, 9, 0]])
